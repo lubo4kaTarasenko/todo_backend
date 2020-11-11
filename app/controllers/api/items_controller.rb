b505c0481b2e901d4df0ec16c44a263754bf08ff
luba.t <lubo4ka.boiko@gmail.com>
2020-11-11 19:03:29 +0200

    fix json parser

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -5,12 +5,14 @@ class Api::ItemsController < ActionController::API
 
   def create
     json = params.permit(:text, :check, :color)
+    json = JSON.parse(request.body.read).symbolize_keys if json.empty?
     item = Item.create(json.merge(token: params[:token]))
     render json: { item: item.values }
   end
 
   def update
     json = params.permit(:id, :text, :check, :color)
+    json = JSON.parse(request.body.read).symbolize_keys if json.empty?
     result = Item.where(token: params[:token], id: params[:id]).update_all(json.to_h)
     render json: { result: result }
   end
